a3ce8da25a1c5e72f0c0c7f5d7d32e164017ee35
Shawn Halayka <sjhalayka@gmail.com>
2025-03-30 20:00:41 -0600

    fixed mark_colliding_bullets

--- main.cpp
+++ main.cpp
@@ -5416,11 +5416,18 @@ void mark_colliding_bullets(void)
 			if (isPixelPerfectCollision(enemyBullets[i], allyShips[j]))
 				enemyBullets[i].death_time = GLOBAL_TIME;
 
-	// get rid of enemy bullets that hit enemy ships too (especially background obstacles)
+	// get rid of enemy bullets that hit enemy the foreground
 	for (size_t i = 0; i < enemyBullets.size(); ++i)
+	{
 		for (size_t j = 0; j < enemyShips.size(); ++j)
+		{
+			if (false == enemyShips[j].is_foreground)
+				continue;
+
 			if (isPixelPerfectCollision(enemyBullets[i], enemyShips[j]))
 				enemyBullets[i].death_time = GLOBAL_TIME;
+		}
+	}
 }
 
 void mark_old_bullets(void)
